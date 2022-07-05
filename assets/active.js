8975d06ce13d4e9ef9b29d9b3844284dd10c3578
mzkki <08harismu@gmail.com>
2022-07-05 21:38:14 +0800

    add active feature

---
+++ assets/active.js
@@ -0,0 +1,32 @@
+const navLinks = document.querySelectorAll("#link")
+
+for (link of navLinks) {
+  link.addEventListener('click', function (event) {
+    const target = event.target
+
+    if (target.innerText === 'HOME') {
+      target.setAttribute('class', 'active')
+      navLinks[1].setAttribute('class', '')
+      navLinks[2].setAttribute('class', '')
+      navLinks[3].setAttribute('class', '')
+    }
+    if (target.innerText === 'ABOUT') {
+      target.setAttribute('class', 'active')
+      navLinks[0].setAttribute('class', '')
+      navLinks[2].setAttribute('class', '')
+      navLinks[3].setAttribute('class', '')
+    }
+    if (target.innerText === 'GALLERY') {
+      target.setAttribute('class', 'active')
+      navLinks[0].setAttribute('class', '')
+      navLinks[1].setAttribute('class', '')
+      navLinks[3].setAttribute('class', '')
+    }
+    if (target.innerText === 'CONTACT') {
+      target.setAttribute('class', 'active')
+      navLinks[0].setAttribute('class', '')
+      navLinks[1].setAttribute('class', '')
+      navLinks[2].setAttribute('class', '')
+    }
+  })
+} 
