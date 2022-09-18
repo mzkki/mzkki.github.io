ad771782e82e0c27ddd3a1c692da03d32976c772
mzkki <08harismu@gmail.com>
2022-09-18 21:11:16 +0800

    new bug on hover social media

--- assets/observer.js
+++ assets/observer.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry.isIntersecting)
     if (entry.isIntersecting) {
       entry.target.classList.add('show')
     } else {
