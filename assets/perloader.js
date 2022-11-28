bd2465dc29cb6a16d27c01eb805e644c1037652e
mzkki <08harismu@gmail.com>
2022-11-28 12:17:41 +0800

    fix laoder

--- assets/perloader.js
+++ assets/perloader.js
@@ -2,4 +2,10 @@ const loader = document.getElementById('preloader')
 //
 window.addEventListener('load', () => {
   loader.style.opacity = 0
+
+  setTimeout(hideLoader, 1000)
 })
+
+const hideLoader = () => {
+  loader.style.display = 'none'
+}
